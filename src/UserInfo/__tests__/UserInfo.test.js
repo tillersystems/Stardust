cf3771e2681d62c22ca7d970b85acec54539a8bb
spartDev <troux@tillersystems.com>
2018-09-21 15:13:29 +0200

    test(UserInfo): use shallow instead of mount

--- src/UserInfo/__tests__/UserInfo.test.js
+++ src/UserInfo/__tests__/UserInfo.test.js
@@ -5,26 +5,27 @@ import UserInfo from '..';
 
 describe('<UserInfo />', () => {
   it('should render withouth a problem', () => {
-    const render = mountWithTheme(<UserInfo name="Tony Starck" />);
+    const render = shallowWithTheme(<UserInfo name="Tony Starck" />);
 
-    expect(render).toMatchSnapshot();
+    expect(render.dive()).toMatchSnapshot();
   });
 
   it('should render a user picture', () => {
-    const render = mountWithTheme(
+    const render = shallowWithTheme(
       <UserInfo name="Tony Starck" pictureSrc="http://pictureSrc.com" />,
     );
 
-    expect(render).toMatchSnapshot();
+    expect(render.dive()).toMatchSnapshot();
   });
 
   it('should trigger onClick function when user click', () => {
     const spy = jest.fn();
-    const render = mountWithTheme(
+    const render = shallowWithTheme(
       <UserInfo name="Tony Starck" pictureSrc="http://pictureSrc.com" onClick={spy} />,
     );
 
     render
+      .dive()
       .find('[data-test="DisconnectWrapper-test"]')
       .at(0)
       .simulate('click');
