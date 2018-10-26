4bf92819b503fbf0a4c4b3cc282459d9f79d25b0
spartDev <troux@tillersystems.com>
2018-10-26 10:12:57 +0200

    refactor(modal): refactor modal animation

--- src/Modal/elements.js
+++ src/Modal/elements.js
@@ -2,18 +2,10 @@ import styled from 'styled-components';
 import breakpoint from 'styled-components-breakpoint';
 
 export const Container = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  min-width: 100%;
-  height: 100%;
-  min-height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
 `;
 
 export const Overlay = styled.div`
@@ -38,7 +30,11 @@ export const Dialog = styled.dialog`
   height: ${({ height }) => height};
   width: ${({ width }) => width};
 
-  margin: 0 auto;
+  left: 50% !important;
+  top: 50% !important;
+  margin-left: ${({ width }) => `calc(-${width} / 2)`};
+  margin-top: ${({ height }) => `calc(-${height} / 2)`};
+
   padding: 0 ${({ theme: { dimensions } }) => dimensions.medium};
 
   color: ${({ theme: { palette } }) => palette.darkBlue};
@@ -54,7 +50,7 @@ export const Dialog = styled.dialog`
   /* For small devices */
   ${breakpoint('xs', 'sm')`
     width:90%;
-    margin: 0 auto;
+    margin-left: calc(-90% / 2)};
   `};
 `;
 
