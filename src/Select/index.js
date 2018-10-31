d0bee22346c46997df162609d7f6a9c3e75645e4
spartDev <troux@tillersystems.com>
2018-10-31 09:19:28 +0100

    doc(select): add doc

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,15 +1,29 @@
-import React, { PureComponent } from 'react';
+import React, { Children, PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import posed, { PoseGroup } from 'react-pose';
 
 import { Container, Dropdown, Aside, Prefix, Placeholder } from './elements';
 import Option from './Option';
-import posed, { PoseGroup } from 'react-pose';
 
 /**
- * Defines a RadioGroup component.
+ * Modal
+ *
+ * This component is in charge of displaying
+ * a select
+ *
+ * @param {string} children // Anything that can be rendered: numbers, strings, elements or an array (or fragment).
+ * @param {string} prefix // Add a text aside in the select next the selected value.
+ * @param {string} selectedValue // Pre select the value if defined.
+ * @param {string} show // Boolean set to display or hide options.
+ * @param {string} placeholder // Text written in the select to explicit the differents values.
+ * @param {string} onClick // Function fired when an element of the dropdown is selected. Return the state in parameter.
+ *
+ * @return {jsx}
  */
+
 class Select extends PureComponent {
   static Option = Option;
+
   /** Prop types. */
   static propTypes = {
     children: PropTypes.node,
@@ -38,10 +52,12 @@ class Select extends PureComponent {
   };
 
   /**
-   * Handles mounting in component's lifecycle.
+   * componentDidMount
+   * Handles mounting
+   * in component's lifecycle.
    */
   componentDidMount() {
-    const { children, show, selectedValue, placeholder } = this.props;
+    const { children, selectedValue, placeholder } = this.props;
 
     if (placeholder !== null) {
       this.handleClick(placeholder, '', null);
@@ -55,14 +71,12 @@ class Select extends PureComponent {
         this.handleClick(c, value, aside);
       }
     }
-
-    if (show !== null) {
-      this.setState({ showed: show });
-    }
   }
 
   /**
-   * Handles update in component's lifecycle.
+   * componentDidUpdate
+   * Handles update
+   * in component's lifecycle.
    *
    * @param {Object} prevProps - The component's previous props.
    */
@@ -85,6 +99,13 @@ class Select extends PureComponent {
     }
   }
 
+  /**
+   * Handle Click
+   *
+   * @param {string} placeholder - Text written in the select to explicit the differents values.
+   * @param {string} val - The option value.
+   * @param {string} aside - Icon aside in the select next the selected value.
+   */
   handleClick = (placeholder, val, aside) => {
     const { onClick } = this.props;
     this.setState(
@@ -97,11 +118,17 @@ class Select extends PureComponent {
     );
   };
 
+  /**
+   * Handle Toggle
+   */
   toggleShow = () => {
     const { showed } = this.state;
     this.setState({ showed: !showed });
   };
 
+  /**
+   * Handle Hide
+   */
   handleHide = e => {
     if (!e.currentTarget.contains(e.relatedTarget)) {
       this.setState({ showed: false });
@@ -117,40 +144,41 @@ class Select extends PureComponent {
     const { children, prefix } = this.props;
     const { value, placeholder, showed, aside } = this.state;
 
-    // const options = React.Children.map(children, option => option);
-    const optionsCustom = React.Children.map(children, option => (
-      <Select.Option
-        value={option.props.value}
-        aside={option.props.aside}
-        onClick={this.handleClick}
-      >
-        {option.props.children}
+    const options = Children.map(children, ({ props: { value, aside, children } }) => (
+      <Select.Option value={value} aside={aside} onClick={this.handleClick}>
+        {children}
       </Select.Option>
     ));
+
     return (
       <Container tabIndex="0" onClick={this.toggleShow} onBlur={this.handleHide}>
         {prefix && <Prefix>{prefix}</Prefix>}
         {aside && <Aside>{aside}</Aside>}
         {value === '' ? <Placeholder>{placeholder}</Placeholder> : placeholder}
         <PoseGroup>
-          {showed && <DropdownAnimation key="DropdownAnimation">{optionsCustom}</DropdownAnimation>}
+          {showed && <DropdownAnimation key="DropdownAnimation">{options}</DropdownAnimation>}
         </PoseGroup>
       </Container>
     );
   }
 }
 
+/**
+ * Animation
+ */
 const DropdownAnimation = posed(Dropdown)({
   enter: {
     opacity: 1,
     scaleY: 1,
+    transition: {
+      scaleY: { type: 'spring', stiffness: 900, damping: 30 },
+      default: { duration: 150 },
+    },
   },
   exit: {
     opacity: 0,
     scaleY: 0,
-    transition: {
-      opacity: { ease: 'easeOut', duration: 250 },
-    },
+    transition: { duration: 150 },
   },
 });
 
