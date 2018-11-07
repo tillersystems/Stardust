4ea808d12b0b7581f8973ffccfd4ec9ae69cefff
spartDev <troux@tillersystems.com>
2018-11-07 16:39:53 +0100

    test(Modal): improve code coverage

--- src/Modal/__tests__/Modal.test.js
+++ src/Modal/__tests__/Modal.test.js
@@ -15,6 +15,11 @@ describe('<Modal />', () => {
 
     expect(render).toMatchSnapshot();
   });
+
+  it('should have default onOverlayClick', () => {
+    expect(Modal.defaultProps.onOverlayClick()).toMatchSnapshot();
+  });
+
   it('should render open modal withouth a problem', () => {
     const render = mountWithTheme(
       <Modal active width="50rem" height="35rem">
@@ -26,15 +31,36 @@ describe('<Modal />', () => {
 
     expect(render).toMatchSnapshot();
   });
-  it('should render open modal with overlay clickable withouth a problem', () => {
+
+  it('should render open modal width a title', () => {
     const render = mountWithTheme(
-      <Modal width="50rem" height="35rem" overlayClick={() => {}}>
-        <Modal.Header>Header</Modal.Header>
+      <Modal active width="50rem" height="35rem">
+        <Modal.Header>
+          <Modal.Title>My title</Modal.Title>
+        </Modal.Header>
         <Modal.Body>Body</Modal.Body>
         <Modal.Footer>Footer</Modal.Footer>
       </Modal>,
     );
 
     expect(render).toMatchSnapshot();
+  });
+
+  it('should call overlayClick callback', () => {
+    const spy = jest.fn();
+    const render = mountWithTheme(
+      <Modal active width="50rem" height="35rem" onOverlayClick={spy}>
+        <Modal.Header>Header</Modal.Header>
+        <Modal.Body>Body</Modal.Body>
+        <Modal.Footer>Footer</Modal.Footer>
+      </Modal>,
+    );
+
+    render
+      .find('[data-test="overlay"]')
+      .first()
+      .simulate('click');
+
+    expect(spy).toHaveBeenCalled();
   });
 });
