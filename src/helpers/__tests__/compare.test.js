c076a57462719678ee547e833fea85817f8251f8
spartDev <troux@tillersystems.com>
2018-11-26 17:13:48 +0100

    fix(table): rewrite test

--- src/helpers/__tests__/compare.test.js
+++ src/helpers/__tests__/compare.test.js
@@ -2,15 +2,24 @@ import compare from '../compare';
 
 describe('sortElementsInArray helpers', () => {
   it('should return 0', () => {
-    const expectedValue = compare(1, 1);
-    expect(expectedValue).toMatchSnapshot();
+    const compareNumber = compare(1, 1);
+    expect(compareNumber).toBe(0);
+
+    const compareString = compare('Anakin', 'Anakin');
+    expect(compareString).toBe(0);
   });
   it('should return -1', () => {
-    const expectedValue = compare(2, 1);
-    expect(expectedValue).toMatchSnapshot();
+    const compareNumber = compare(2, 1);
+    expect(compareNumber).toBe(-1);
+
+    const compareString = compare('Luke', 'Anakin');
+    expect(compareString).toBe(-1);
   });
   it('should return 1', () => {
-    const expectedValue = compare(1, 2);
-    expect(expectedValue).toMatchSnapshot();
+    const compareNumber = compare(1, 2);
+    expect(compareNumber).toBe(1);
+
+    const compareString = compare('Anakin', 'Luke');
+    expect(compareString).toBe(1);
   });
 });
