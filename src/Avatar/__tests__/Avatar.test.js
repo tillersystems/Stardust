1c2e45e9b3067261fc1a795bcdaf4daae3d0e403
Thomas Roux <troux@tillersystems.com>
2018-12-14 13:33:03 +0100

    test(avatar): refactor unit test with react-testing-library instead of enzyme (#231)

--- src/Avatar/__tests__/Avatar.test.js
+++ src/Avatar/__tests__/Avatar.test.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import 'jest-styled-components';
 
 import Avatar from '..';
 
 describe('<Avatar />', () => {
-  it('should render withouth a problem', () => {
-    const render = shallowWithTheme(<Avatar name="James Bond" />);
-    expect(render.dive()).toMatchSnapshot();
+  test('should render withouth a problem', () => {
+    const { container } = render(<Avatar name="James Bond" />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render with a custom user image', () => {
-    const render = shallowWithTheme(<Avatar name="James Bond" src="http://urlofmyimage.com" />);
-    expect(render.dive()).toMatchSnapshot();
+  test('should render with a custom user image', () => {
+    const { container } = render(<Avatar name="James Bond" src="http://urlofmyimage.com" />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render withouth a custom size', () => {
-    const render = shallowWithTheme(<Avatar name="James Bond" size={4.1} />);
-    expect(render.dive()).toMatchSnapshot();
+  test('should render withouth a custom size', () => {
+    const { container } = render(
+      <Avatar name="James Bond" size={4.1} src="http://urlofmyimage.com" />,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
