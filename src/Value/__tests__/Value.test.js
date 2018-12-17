98e8c0c94ecf3e047c41bf5ebe850db7a432032e
Evan Burrell <burrelle@oregonstate.edu>
2018-12-17 13:45:34 +0000

    test(Value) use container.firstChild to avoid extra div in snapshot

--- src/Value/__tests__/Value.test.js
+++ src/Value/__tests__/Value.test.js
@@ -4,16 +4,16 @@ import Value from '..';
 describe('<Value />', () => {
   test('should render withouth a problem', () => {
     const { container } = render(<Value>0</Value>);
-    expect(container).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('should render as negative', () => {
     const { container } = render(<Value negative>-10</Value>);
-    expect(container).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   test('should render as postive', () => {
     const { container } = render(<Value positive>+10</Value>);
-    expect(container).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
