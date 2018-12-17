b54a21ab893c7231559160abb5e49ee4dd985353
Evan Burrell <burrelle@oregonstate.edu>
2018-12-17 13:07:05 +0000

    test(Logo) use react-testing-library instead of enzyme
    Update to use test method instead of it method

--- src/Logo/__tests__/Logo.test.js
+++ src/Logo/__tests__/Logo.test.js
@@ -4,19 +4,19 @@ import 'jest-styled-components';
 import Logo from '..';
 
 describe('<Logo />', () => {
-  it('should render withouth a problem', () => {
+  test('should render withouth a problem', () => {
     const { container } = render(<Logo />);
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render with custom width & height', () => {
+  test('should render with custom width & height', () => {
     const { getByTestId } = render(<Logo width="300" height="80" />);
     const logoNode = getByTestId('logo');
     expect(logoNode).toHaveAttribute('width', '300');
     expect(logoNode).toHaveAttribute('height', '80');
   });
 
-  it('should render with custom color', () => {
+  test('should render with custom color', () => {
     const color = 'hsl(217, 89%, 61%)';
     const { getByTestId } = render(<Logo color={color} />);
     const logoPathNode = getByTestId('logoPath');
