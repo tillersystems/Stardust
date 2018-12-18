5a1eb604f9e3cca02d6f79024d2d6a46b0f633b8
Thomas Roux <troux@tillersystems.com>
2018-12-18 17:55:44 +0100

    test(Button): use react-testing-library instead of enzyme (#280)

--- src/Button/__tests__/helpers.test.js
+++ src/Button/__tests__/helpers.test.js
@@ -3,37 +3,37 @@ import { getAppearance, getSize } from '../helpers';
 
 describe('<Button /> -- helpers', () => {
   describe('getAppearance', () => {
-    it('should return default css appearance', () => {
+    test('should return default css appearance', () => {
       const expectedCss = getAppearance(Theme, 'default');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return primary css appearance', () => {
+    test('should return primary css appearance', () => {
       const expectedCss = getAppearance(Theme, 'primary');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return secondary css appearance', () => {
+    test('should return secondary css appearance', () => {
       const expectedCss = getAppearance(Theme, 'secondary');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return success css appearance', () => {
+    test('should return success css appearance', () => {
       const expectedCss = getAppearance(Theme, 'success');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return danger css appearance', () => {
+    test('should return danger css appearance', () => {
       const expectedCss = getAppearance(Theme, 'danger');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return google css appearance', () => {
+    test('should return google css appearance', () => {
       const expectedCss = getAppearance(Theme, 'google');
 
       expect(expectedCss).toMatchSnapshot();
@@ -41,19 +41,19 @@ describe('<Button /> -- helpers', () => {
   });
 
   describe('getSize', () => {
-    it('should return default css size', () => {
+    test('should return default css size', () => {
       const expectedCss = getSize(Theme, 'default');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return small css size', () => {
+    test('should return small css size', () => {
       const expectedCss = getSize(Theme, 'small');
 
       expect(expectedCss).toMatchSnapshot();
     });
 
-    it('should return large css size', () => {
+    test('should return large css size', () => {
       const expectedCss = getSize(Theme, 'large');
 
       expect(expectedCss).toMatchSnapshot();
