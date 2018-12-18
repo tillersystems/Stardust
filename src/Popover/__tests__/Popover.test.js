8ab494f8da9d060ae006825f19e6730aa460a8a0
spartDev <troux@tillersystems.com>
2018-12-18 19:54:39 +0100

    test(Popover): change jest it by test

--- src/Popover/__tests__/Popover.test.js
+++ src/Popover/__tests__/Popover.test.js
@@ -3,20 +3,20 @@ import React from 'react';
 import Popover from '..';
 
 describe('<Popover />', () => {
-  it('should render withouth a problem', () => {
+  test('should render withouth a problem', () => {
     const { container } = render(<Popover>Children</Popover>);
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render open popover withouth a problem', () => {
+  test('should render open popover withouth a problem', () => {
     const { getByTestId } = render(<Popover active>Children</Popover>);
     const popoverNode = getByTestId('popover');
 
     expect(popoverNode).toBeInTheDocument();
   });
 
-  it('should render with a different width', () => {
+  test('should render with a different width', () => {
     const width = '10rem';
     const { getByTestId } = render(
       <Popover active width={width}>
