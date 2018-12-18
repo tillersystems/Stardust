a2c61c62621204029db59e807b52a3cbb9da23e7
Thomas Roux <troux@tillersystems.com>
2018-12-18 11:40:53 +0100

    test(Counter): use react-testing-library instead of enzyme (#284)

--- src/Counter/__tests__/Increment.test.js
+++ src/Counter/__tests__/Increment.test.js
@@ -1,105 +1,78 @@
 import React from 'react';
-import 'jest-styled-components';
+import { fireEvent } from 'react-testing-library';
 
 import Counter from '..';
-import { Button } from '../..';
 
 describe('<Counter />', () => {
-  it('should render withouth a problem', () => {
-    const render = shallowWithTheme(<Counter />);
+  test('should render withouth a problem', () => {
+    const { container } = render(<Counter />);
 
-    expect(render.dive()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should increment counter', () => {
-    const render = mountWithTheme(
+  test('should increment counter', () => {
+    const { getByTestId } = render(
       <Counter step={1} max={100} min={0} value={0} appearance="secondary" width="5rem" />,
     );
+    const incrementButton = getByTestId('increment');
+    const fakeinputValue = getByTestId('fakeinput');
 
-    render
-      .find(Button)
-      .at(1)
-      .simulate('click');
+    fireEvent.click(incrementButton);
 
-    const fakeinputValue = render
-      .find('[data-test="fakeinput"]')
-      .last()
-      .text();
-
-    expect(fakeinputValue).toEqual('1');
+    expect(fakeinputValue).toHaveTextContent('1');
   });
 
-  it('should decrement counter', () => {
-    const render = mountWithTheme(
+  test('should decrement counter', () => {
+    const { getByTestId } = render(
       <Counter step={1} max={100} min={0} value={10} appearance="secondary" width="5rem" />,
     );
+    const decrementButton = getByTestId('decrement');
+    const fakeinputValue = getByTestId('fakeinput');
 
-    render
-      .find(Button)
-      .at(0)
-      .simulate('click');
+    fireEvent.click(decrementButton);
 
-    const fakeinputValue = render
-      .find('[data-test="fakeinput"]')
-      .last()
-      .text();
-
-    expect(fakeinputValue).toEqual('9');
+    expect(fakeinputValue).toHaveTextContent('9');
   });
 
-  it('should decrement counter until the min value', () => {
-    const render = mountWithTheme(
+  test('should decrement counter until the min value', () => {
+    const { getByTestId } = render(
       <Counter step={1} max={100} min={0} value={99} appearance="secondary" width="5rem" />,
     );
+    const incrementButton = getByTestId('increment');
+    const fakeinputValue = getByTestId('fakeinput');
 
-    render
-      .find(Button)
-      .at(1)
-      .simulate('click');
+    expect(fakeinputValue).toHaveTextContent('99');
 
-    const fakeinputValue = render
-      .find('[data-test="fakeinput"]')
-      .last()
-      .text();
+    fireEvent.click(incrementButton);
 
-    expect(fakeinputValue).toEqual('100');
+    expect(fakeinputValue).toHaveTextContent('100');
 
-    render
-      .find(Button)
-      .at(1)
-      .simulate('click');
+    fireEvent.click(incrementButton);
 
-    expect(fakeinputValue).toEqual('100');
+    expect(fakeinputValue).toHaveTextContent('100');
   });
 
-  it('should decrement counter until the min value', () => {
-    const render = mountWithTheme(
+  test('should decrement counter until the min value', () => {
+    const { getByTestId } = render(
       <Counter step={1} max={100} min={0} value={1} appearance="secondary" width="5rem" />,
     );
+    const decrementButton = getByTestId('decrement');
+    const fakeinputValue = getByTestId('fakeinput');
 
-    render
-      .find(Button)
-      .at(0)
-      .simulate('click');
+    expect(fakeinputValue).toHaveTextContent('1');
 
-    const fakeinputValue = render
-      .find('[data-test="fakeinput"]')
-      .last()
-      .text();
+    fireEvent.click(decrementButton);
 
-    expect(fakeinputValue).toEqual('0');
+    expect(fakeinputValue).toHaveTextContent('0');
 
-    render
-      .find(Button)
-      .at(0)
-      .simulate('click');
+    fireEvent.click(decrementButton);
 
-    expect(fakeinputValue).toEqual('0');
+    expect(fakeinputValue).toHaveTextContent('0');
   });
 
-  it('should call onIncrement callback function', () => {
+  test('should call onIncrement callback function', () => {
     const spy = jest.fn();
-    const render = mountWithTheme(
+    const { getByTestId } = render(
       <Counter
         step={1}
         max={100}
@@ -110,18 +83,16 @@ describe('<Counter />', () => {
         width="5rem"
       />,
     );
+    const incrementButton = getByTestId('increment');
 
-    render
-      .find(Button)
-      .at(1)
-      .simulate('click');
+    fireEvent.click(incrementButton);
 
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  it('should call onDecrement callback function', () => {
+  test('should call onDecrement callback function', () => {
     const spy = jest.fn();
-    const render = mountWithTheme(
+    const { getByTestId } = render(
       <Counter
         step={1}
         max={100}
@@ -132,11 +103,9 @@ describe('<Counter />', () => {
         width="5rem"
       />,
     );
+    const decrementButton = getByTestId('decrement');
 
-    render
-      .find(Button)
-      .at(0)
-      .simulate('click');
+    fireEvent.click(decrementButton);
 
     expect(spy).toHaveBeenCalledTimes(1);
   });
