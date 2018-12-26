9399dc4a2dd1715bc64362dfd4794151986dab8b
Thomas Roux <troux@tillersystems.com>
2018-12-26 13:24:18 +0100

    test(RadioButton): use react-testing-library instead of enzyme (#312)

--- src/RadioButton/__tests__/RadioButton.test.js
+++ src/RadioButton/__tests__/RadioButton.test.js
@@ -5,48 +5,48 @@ import RadioButton from '..';
 
 describe('<RadioButton />', () => {
   it('should render without a problem', () => {
-    const render = shallowWithTheme(<RadioButton id="test">test</RadioButton>);
+    const { container } = render(<RadioButton id="test">test</RadioButton>);
 
-    expect(render.dive()).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   it('should render without a problem when radio is selected and enabled', () => {
-    const render = shallowWithTheme(
+    const { container } = render(
       <RadioButton id="test" value="apple" selectedValue="apple">
         test
       </RadioButton>,
     );
 
-    expect(render.dive()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render without a problem when radio is not selected and enabled', () => {
-    const render = shallowWithTheme(
+    const { container } = render(
       <RadioButton id="test" value="apple" selectedValue="apricot">
         test
       </RadioButton>,
     );
 
-    expect(render.dive()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render without a problem when radio is selected  and disabled', () => {
-    const render = shallowWithTheme(
+    const { container } = render(
       <RadioButton id="test" value="apple" selectedValue="apple" disabled>
         test
       </RadioButton>,
     );
 
-    expect(render.dive()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render without a problem when radio is not selected and disabled', () => {
-    const render = shallowWithTheme(
+    const { container } = render(
       <RadioButton id="test" value="apple" selectedValue="apricot" disabled>
         test
       </RadioButton>,
     );
 
-    expect(render.dive()).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
