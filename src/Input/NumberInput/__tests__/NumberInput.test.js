80255131a6a41f4ea82a7b50b18a3825ea4d4e75
spartDev <troux@tillersystems.com>
2019-01-02 15:38:06 +0100

    test(NumberInput): use react-testing-library instead of enzyme

--- src/Input/NumberInput/__tests__/NumberInput.test.js
+++ src/Input/NumberInput/__tests__/NumberInput.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import 'jest-styled-components';
+import { fireEvent } from 'react-testing-library';
 
 import NumberInput from '..';
+import Theme from '../../../Theme';
 
 describe('<NumberInput />', () => {
   let addEventListener = null;
@@ -14,204 +15,216 @@ describe('<NumberInput />', () => {
     document.addEventListener = addEventListener;
   });
 
-  it('should render withouth a problem', () => {
-    const render = mountWithTheme(<NumberInput />);
-    expect(render).toMatchSnapshot();
+  test('should render withouth a problem', () => {
+    const { container } = render(<NumberInput />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render withouth a problem when focused and unfocused', () => {
-    const render = mountWithTheme(<NumberInput />);
+  test('should render withouth a problem when focused and unfocused', () => {
+    const { container, getByTestId } = render(<NumberInput />);
+    const inputeNode = getByTestId('input');
 
-    render.find('input').simulate('focus');
-    expect(render).toMatchSnapshot();
+    fireEvent.focus(inputeNode);
+    expect(container.firstChild).toMatchSnapshot();
 
-    render.find('input').simulate('blur');
-    expect(render).toMatchSnapshot();
+    fireEvent.blur(inputeNode);
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render withouth a problem when value changed', () => {
-    const render = mountWithTheme(<NumberInput />);
+  test('allow the user to type numbers', () => {
+    const { getByTestId } = render(<NumberInput />);
+    const inputeNode = getByTestId('input');
+    const inputValue = '123';
 
-    render
-      .find('input')
-      .simulate('focus')
-      .simulate('change', { target: { name: 'input', value: '123' } });
-    expect(render).toMatchSnapshot();
+    fireEvent.change(inputeNode, { target: { value: inputValue } });
+
+    expect(inputeNode.value).toEqual(inputValue);
   });
 
-  it('should call change handler when controlled and value changed', () => {
+  test('should call change handler when controlled and value changed', () => {
     const spy = jest.fn();
-    const render = mountWithTheme(<NumberInput onChange={spy} />);
+    const { getByTestId } = render(<NumberInput onChange={spy} />);
+    const inputeNode = getByTestId('input');
+    const inputValue = '123';
 
-    render
-      .find('input')
-      .simulate('focus')
-      .simulate('change', { target: { name: 'input', value: '123' } });
+    fireEvent.change(inputeNode, { target: { value: inputValue } });
 
     expect(spy).toHaveBeenCalledWith(123);
   });
 
-  it('should not call change handler when controlled and disabled and text changed', () => {
+  test('should not call change handler when controlled and disabled and text changed', () => {
     const spy = jest.fn();
+    const { getByTestId } = render(<NumberInput disabled onChange={spy} />);
+    const inputeNode = getByTestId('input');
+    const inputValue = '123';
 
-    const render = mountWithTheme(<NumberInput disabled onChange={spy} />);
-
-    render
-      .find('input')
-      .simulate('focus')
-      .simulate('change', { target: { name: 'input', value: 'hello' } });
+    fireEvent.change(inputeNode, { target: { value: inputValue } });
 
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('should render withouth a problem when given a width', () => {
-    const render = mountWithTheme(<NumberInput width="123rem" />);
-    expect(render).toMatchSnapshot();
+  test('should have a custom width', () => {
+    const width = '123rem';
+    const { container } = render(<NumberInput width={width} />);
+
+    expect(container.firstChild).toHaveStyleRule('width', width);
   });
 
-  it('should render withouth a problem when fluid', () => {
-    const render = mountWithTheme(<NumberInput fluid />);
-    expect(render).toMatchSnapshot();
+  test('should render as fluid', () => {
+    const { container } = render(<NumberInput fluid />);
+
+    expect(container.firstChild).toHaveStyleRule('width', '100%');
   });
 
-  it('should render input without problem when given an icon label', () => {
-    const render = mountWithTheme(<NumberInput label={{ icon: 'cog' }} />);
-    expect(render).toMatchSnapshot();
+  test('should have an icon label', () => {
+    const { container } = render(<NumberInput label={{ icon: 'cog' }} />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render input without problem when given an icon label and label position', () => {
-    const render = mountWithTheme(<NumberInput label={{ icon: 'cog' }} labelPosition="right" />);
-    expect(render).toMatchSnapshot();
+  test('should have an icon icon label and label position', () => {
+    const { container } = render(<NumberInput label={{ icon: 'cog' }} labelPosition="right" />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render input without problem when given a text label', () => {
-    const render = mountWithTheme(<NumberInput label={{ text: 'Pickle Rick' }} />);
-    expect(render).toMatchSnapshot();
+  test('should have a text label', () => {
+    const { container } = render(<NumberInput label={{ text: 'Pickle Rick' }} />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render disabled without problem', () => {
-    const render = mountWithTheme(<NumberInput disabled />);
-    expect(render).toMatchSnapshot();
+  test('should render disabled without problem', () => {
+    const { container, getByTestId } = render(<NumberInput disabled />);
+    const inputeNode = getByTestId('input');
+
+    expect(container.firstChild).toHaveStyleRule('opacity', '0.4');
+    expect(container.firstChild).toHaveStyleRule('border-color', Theme.palette.lightGrey);
+    expect(inputeNode.disabled).toBeTruthy();
   });
 
-  it('should render loading without problem', () => {
-    const render = mountWithTheme(<NumberInput loading />);
-    expect(render).toMatchSnapshot();
+  test('should have a status info', () => {
+    const { container, getByTestId } = render(<NumberInput info />);
+    const statusNode = getByTestId('status');
+
+    expect(container.firstChild).toHaveStyleRule('border-color', Theme.palette.primary.default);
+    expect(statusNode).toBeInTheDocument();
   });
 
-  it('should render info without problem', () => {
-    const render = mountWithTheme(<NumberInput info />);
-    expect(render).toMatchSnapshot();
+  test('should have a status success', () => {
+    const { container, getByTestId } = render(<NumberInput success />);
+    const statusNode = getByTestId('status');
+
+    expect(container.firstChild).toHaveStyleRule('border-color', Theme.palette.success.default);
+    expect(statusNode).toBeInTheDocument();
   });
 
-  it('should render success without problem', () => {
-    const render = mountWithTheme(<NumberInput success />);
-    expect(render).toMatchSnapshot();
+  test('should have a status warning', () => {
+    const { container, getByTestId } = render(<NumberInput warning />);
+    const statusNode = getByTestId('status');
+
+    expect(container.firstChild).toHaveStyleRule('border-color', Theme.palette.warning.default);
+    expect(statusNode).toBeInTheDocument();
   });
 
-  it('should render warning without problem', () => {
-    const render = mountWithTheme(<NumberInput warning />);
-    expect(render).toMatchSnapshot();
+  test('should have a status error', () => {
+    const { container, getByTestId } = render(<NumberInput error />);
+    const statusNode = getByTestId('status');
+
+    expect(container.firstChild).toHaveStyleRule('border-color', Theme.palette.failure.default);
+    expect(statusNode).toBeInTheDocument();
   });
 
-  it('should render error without problem', () => {
-    const render = mountWithTheme(<NumberInput error />);
-    expect(render).toMatchSnapshot();
-  });
-
-  it('should update the component when controlled value is changed', () => {
-    const render = mountWithTheme(<NumberInput />);
-
-    expect(render).toMatchSnapshot();
-
-    render.setProps({ value: 123 });
-    expect(render).toMatchSnapshot();
-  });
-
-  it('should increase amount when focused and key up pressed', () => {
+  test('should increase amount when focused and key up pressed', () => {
     let keyDownListener;
     document.addEventListener = jest.fn((event, listener) => {
       if (event === 'keydown') keyDownListener = listener;
     });
 
     const spy = jest.fn();
-    const render = mountWithTheme(<NumberInput onChange={spy} />);
+    const { getByTestId } = render(<NumberInput onChange={spy} />);
+    const inputeNode = getByTestId('input');
 
     if (!keyDownListener) throw new Error('Expected keyDownListener not to be undefined.');
 
-    render.find('input').simulate('focus');
+    fireEvent.focus(inputeNode);
     keyDownListener({ code: 'ArrowUp' });
+
     expect(spy).toHaveBeenCalledWith(1);
-
-    render.unmount();
   });
 
-  it('should not increase amount when not focused and key up pressed', () => {
+  test('should not increase amount when not focused and key up pressed', () => {
     let keyDownListener;
     document.addEventListener = jest.fn((event, listener) => {
       if (event === 'keydown') keyDownListener = listener;
     });
 
     const spy = jest.fn();
-    mountWithTheme(<NumberInput onChange={spy} />);
+    const { getByTestId } = render(<NumberInput onChange={spy} />);
+    const inputeNode = getByTestId('input');
 
     if (!keyDownListener) throw new Error('Expected keyDownListener not to be undefined.');
 
+    fireEvent.blur(inputeNode);
     keyDownListener({ code: 'ArrowUp' });
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('should decrease amount when focused and key up pressed', () => {
+  test('should decrease amount when focused and key down pressed', () => {
     let keyDownListener;
     document.addEventListener = jest.fn((event, listener) => {
       if (event === 'keydown') keyDownListener = listener;
     });
 
     const spy = jest.fn();
-    const render = mountWithTheme(<NumberInput onChange={spy} />);
+    const { getByTestId } = render(<NumberInput onChange={spy} />);
+    const inputeNode = getByTestId('input');
 
     if (!keyDownListener) throw new Error('Expected keyDownListener not to be undefined.');
 
-    render.find('input').simulate('focus');
+    fireEvent.focus(inputeNode);
     keyDownListener({ code: 'ArrowDown' });
+
     expect(spy).toHaveBeenCalledWith(-1);
   });
 
-  it('should not decrease amount when not focused and key up pressed', () => {
+  test('should not decrease amount when not focused and key up pressed', () => {
     let keyDownListener;
     document.addEventListener = jest.fn((event, listener) => {
       if (event === 'keydown') keyDownListener = listener;
     });
 
     const spy = jest.fn();
-    mountWithTheme(<NumberInput onChange={spy} />);
+    const { getByTestId } = render(<NumberInput onChange={spy} />);
+    const inputeNode = getByTestId('input');
 
     if (!keyDownListener) throw new Error('Expected keyDownListener not to be undefined.');
 
+    fireEvent.blur(inputeNode);
     keyDownListener({ code: 'ArrowDown' });
+
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('should update value when a min is given a input is lower', () => {
+  test('should update value when a min is given a input is lower', () => {
+    const inputValue = '123';
     const spy = jest.fn();
-    const render = mountWithTheme(<NumberInput min={456} onChange={spy} />);
+    const { getByTestId } = render(<NumberInput min={456} onChange={spy} />);
+    const inputeNode = getByTestId('input');
 
-    render
-      .find('input')
-      .simulate('focus')
-      .simulate('change', { target: { name: 'input', value: '123' } });
+    fireEvent.change(inputeNode, { target: { value: inputValue } });
 
     expect(spy).toHaveBeenCalledWith(456);
   });
 
-  it('should update value when a max is given a input is greater', () => {
+  test('should update value when a max is given a input is greater', () => {
+    const inputValue = '789';
     const spy = jest.fn();
-    const render = mountWithTheme(<NumberInput max={456} onChange={spy} />);
+    const { getByTestId } = render(<NumberInput max={456} onChange={spy} />);
+    const inputeNode = getByTestId('input');
 
-    render
-      .find('input')
-      .simulate('focus')
-      .simulate('change', { target: { name: 'input', value: '789' } });
+    fireEvent.change(inputeNode, { target: { value: inputValue } });
 
     expect(spy).toHaveBeenCalledWith(456);
   });
