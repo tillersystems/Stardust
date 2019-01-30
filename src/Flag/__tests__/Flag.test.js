8b3176cc46e3b3f3b2984671d19e14be0486d3fc
Sun-tea <xthi.ngu@gmail.com>
2019-01-30 15:38:49 +0100

    test(Flag): add test for rounded corners

--- src/Flag/__tests__/Flag.test.js
+++ src/Flag/__tests__/Flag.test.js
@@ -17,4 +17,14 @@ describe('<Flag />', () => {
     expect(flagSvgNode).toHaveAttribute('width', customSize);
     expect(flagSvgNode).toHaveAttribute('height', customSize);
   });
+
+  test('should render with rounded corners', () => {
+    // mock getBBox() which doesn't exist in jsdom to get the dimensions and position of the flag
+    if (!SVGElement.prototype.getBBox) {
+      SVGElement.prototype.getBBox = () => ({ height: 336, width: 450, x: 31, y: 88 });
+    }
+
+    const { container } = render(<Flag name="fr" rounded />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
