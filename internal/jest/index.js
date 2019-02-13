972da3cd62a7904b7aa3fa8e4c99f669c1ce2b68
Sun-tea <xthi.ngu@gmail.com>
2019-02-13 16:26:40 +0100

    fix(test): add polyfill on tests

--- internal/jest/index.js
+++ internal/jest/index.js
@@ -1,5 +1,6 @@
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@babel/polyfill';
 
 import { shallowWithTheme, mountWithTheme, render } from './utils';
 
