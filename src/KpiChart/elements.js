49cf4881dd54879b1259281400274b309135025b
Sun-tea <xthi.ngu@gmail.com>
2019-03-06 11:35:44 +0100

    fix(KpiChart): update Title style

--- src/KpiChart/elements.js
+++ src/KpiChart/elements.js
@@ -4,7 +4,9 @@ export const Title = styled.div`
   font-weight: 600;
   font-size: 1.6rem;
 
-  padding-bottom: 0.5rem;
+  padding-bottom: 1.5rem;
 
-  border-bottom: 1px dotted ${({ theme: { palette } }) => palette.darkBlue};
+  border-bottom: 3px solid ${({ theme: { palette } }) => palette.primary.default};
+  display: flex;
+  align-items: center;
 `;
