4ebf59de65c4d0b3c87fdc1ce1ac645db3bcadc2
Artikodin <artium.wasse@gmail.com>
2019-04-02 10:17:44 +0200

    feat(Tooltip): Change Tooltip to use the tippy library (#455)
    * chore(package): Add Tippy library to project
    
    * feat(Tooltip): delete helpers
    
    * feat(Tooltip): Adapt storybook to the new tooltip
    
    * feat(Tooltip): Change Tooltip to use the tippy library

--- src/Tooltip/__stories__/Tooltip.stories.js
+++ src/Tooltip/__stories__/Tooltip.stories.js
@@ -7,8 +7,7 @@ import { Button, Tooltip } from '../..';
 storiesOf('Tooltip', module)
   .addDecorator(withKnobs)
   .add('default', () => {
-    const topValue = boolean('Top', false, 'State');
-    const hoverValue = boolean('Hover', false, 'State');
+    const arrow = boolean('Arrow', true, 'State');
     const appearance = select(
       'Appearance',
       {
@@ -16,6 +15,17 @@ storiesOf('Tooltip', module)
         light: 'light',
       },
       'dark',
+      'State',
+    );
+
+    const boundary = select(
+      'Boundary',
+      {
+        scrollParent: 'scrollParent',
+        window: 'window',
+        viewport: 'viewport',
+      },
+      'window',
       'State',
     );
 
@@ -30,36 +40,55 @@ storiesOf('Tooltip', module)
       },
       'Dimensions',
     );
-
-    const arrowPositionXValue = number(
-      'arrowPositionX',
-      50,
+    const placement = select(
+      'Placement',
       {
-        range: true,
-        min: 5,
-        max: 90,
-        step: 1,
+        top: 'top',
+        'top-start': 'top-start',
+        'top-end': 'top-end',
+        right: 'right',
+        'right-start': 'right-start',
+        'right-end': 'right-end',
+        bottom: 'bottom',
+        'bottom-start': 'bottom-start',
+        'bottom-end': 'bottom-end',
+        left: 'left',
+        'left-start': 'left-start',
+        'left-end': 'left-end',
       },
-      'Dimensions',
+      'top',
+      'State',
+    );
+
+    const trigger = select(
+      'trigger',
+      {
+        mouseenter: 'mouseenter',
+        focus: 'focus',
+        click: 'click',
+        manual: 'manual',
+      },
+      'click',
+      'State',
     );
 
     return (
       <Tooltip
-        top={topValue}
         appearance={appearance}
-        hover={hoverValue}
-        width={`${widthValue}rem`}
-        arrowPositionX={`${(widthValue * arrowPositionXValue) / 100}rem`}
-        title="Ventes nettes (ventes brutes moins les réductions et les annulations) plus les taxes
+        arrow={arrow}
+        boundary={boundary}
+        maxWidth={`${widthValue}rem`}
+        content="Ventes nettes (ventes brutes moins les réductions et les annulations) plus les taxes
         sur la période séléctionnée."
+        placement={placement}
+        trigger={trigger}
       >
         <Button appearance="primary">Show Tooltip</Button>
       </Tooltip>
     );
   })
   .add('controlled', () => {
-    const topValue = boolean('Top', false, 'State');
-    const activeValue = boolean('Show', false, 'State');
+    const arrow = boolean('Arrow', true, 'State');
     const appearance = select(
       'Appearance',
       {
@@ -70,9 +99,20 @@ storiesOf('Tooltip', module)
       'State',
     );
 
+    const boundary = select(
+      'Boundary',
+      {
+        scrollParent: 'scrollParent',
+        window: 'window',
+        viewport: 'viewport',
+      },
+      'window',
+      'State',
+    );
+
     const widthValue = number(
       'Width',
-      60,
+      28,
       {
         range: true,
         min: 10,
@@ -81,29 +121,53 @@ storiesOf('Tooltip', module)
       },
       'Dimensions',
     );
-
-    const arrowPositionXValue = number(
-      'arrowPositionX',
-      50,
+    const placement = select(
+      'Placement',
       {
-        range: true,
-        min: 5,
-        max: 90,
-        step: 1,
+        top: 'top',
+        'top-start': 'top-start',
+        'top-end': 'top-end',
+        right: 'right',
+        'right-start': 'right-start',
+        'right-end': 'right-end',
+        bottom: 'bottom',
+        'bottom-start': 'bottom-start',
+        'bottom-end': 'bottom-end',
+        left: 'left',
+        'left-start': 'left-start',
+        'left-end': 'left-end',
       },
-      'Dimensions',
+      'top',
+      'State',
     );
+
+    const trigger = select(
+      'trigger',
+      {
+        mouseenter: 'mouseenter',
+        focus: 'focus',
+        click: 'click',
+        manual: 'manual',
+      },
+      'click',
+      'State',
+    );
+
+    const isVisible = boolean('Is visible', true, 'State');
+
     return (
       <Tooltip
-        active={activeValue}
         appearance={appearance}
-        top={topValue}
-        width={`${widthValue}rem`}
-        arrowPositionX={`${(widthValue * arrowPositionXValue) / 100}rem`}
-        title="Ventes nettes (ventes brutes moins les réductions et les annulations) plus les taxes
+        arrow={arrow}
+        boundary={boundary}
+        maxWidth={`${widthValue}rem`}
+        content="Ventes nettes (ventes brutes moins les réductions et les annulations) plus les taxes
         sur la période séléctionnée."
+        placement={placement}
+        trigger={trigger}
+        isVisible={isVisible}
       >
-        <span>Control modal from knobs!</span>
+        <Button appearance="primary">Show Tooltip</Button>
       </Tooltip>
     );
   });
