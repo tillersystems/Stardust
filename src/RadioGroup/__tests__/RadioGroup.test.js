e4f4e82199e61b866aa5d5c67f4353db45e71224
spartdev <troux@tillersystems.com>
2019-05-04 13:10:49 +0200

    test(RadioGroup): swicth test fron enzyme to styled-component

--- src/RadioGroup/__tests__/RadioGroup.test.js
+++ src/RadioGroup/__tests__/RadioGroup.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import 'jest-styled-components';
+import { fireEvent } from 'react-testing-library';
 
 import RadioGroup from '..';
 import { RadioButton } from '../..';
 
 describe('<RadioGroup />', () => {
-  it('should render without a problem', () => {
-    const render = mountWithTheme(
+  test('should render without a problem', () => {
+    const { container } = render(
       <RadioGroup groupName="vegetable">
         <RadioButton value="artichoke" id="artichoke">
           artichoke
@@ -20,11 +20,11 @@ describe('<RadioGroup />', () => {
       </RadioGroup>,
     );
 
-    expect(render).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('should render without a problem when radio is pre-selected', () => {
-    const render = shallowWithTheme(
+  test('should should have selected value by default', () => {
+    const { getByLabelText } = render(
       <RadioGroup groupName="vegetable" selectedValue="artichoke">
         <RadioButton value="artichoke" id="artichoke">
           artichoke
@@ -38,10 +38,74 @@ describe('<RadioGroup />', () => {
       </RadioGroup>,
     );
 
-    expect(render.dive()).toMatchSnapshot();
+    const artichokeNode = getByLabelText('artichoke');
+    const beetrootNode = getByLabelText('beetroot');
+    const pumpkinNode = getByLabelText('beetroot');
+
+    expect(artichokeNode).toHaveAttribute('checked');
+    expect(beetrootNode).not.toHaveAttribute('checked');
+    expect(pumpkinNode).not.toHaveAttribute('checked');
   });
 
-  it("should render without a problem when it's in row", () => {
+  test('should select another value', () => {
+    const { getByLabelText } = render(
+      <RadioGroup groupName="vegetable" selectedValue="artichoke">
+        <RadioButton value="artichoke" id="artichoke">
+          artichoke
+        </RadioButton>
+        <RadioButton value="beetroot" id="beetroot">
+          beetroot
+        </RadioButton>
+        <RadioButton value="pumpkin" id="pumpkin">
+          pumpkin
+        </RadioButton>
+      </RadioGroup>,
+    );
+
+    let artichokeNode = getByLabelText('artichoke');
+    let beetrootNode = getByLabelText('beetroot');
+    let pumpkinNode = getByLabelText('pumpkin');
+
+    expect(artichokeNode).toHaveAttribute('checked');
+    expect(beetrootNode).not.toHaveAttribute('checked');
+    expect(pumpkinNode).not.toHaveAttribute('checked');
+
+    fireEvent.click(beetrootNode);
+
+    artichokeNode = getByLabelText('artichoke');
+    beetrootNode = getByLabelText('beetroot');
+    pumpkinNode = getByLabelText('pumpkin');
+
+    expect(artichokeNode).not.toHaveAttribute('checked');
+    expect(beetrootNode).toHaveAttribute('checked');
+    expect(pumpkinNode).not.toHaveAttribute('checked');
+  });
+
+  test('should have radio button disabled', () => {
+    const { getByLabelText } = render(
+      <RadioGroup groupName="vegetable" selectedValue="artichoke">
+        <RadioButton value="artichoke" id="artichoke" disabled>
+          artichoke
+        </RadioButton>
+        <RadioButton value="beetroot" id="beetroot">
+          beetroot
+        </RadioButton>
+        <RadioButton value="pumpkin" id="pumpkin">
+          pumpkin
+        </RadioButton>
+      </RadioGroup>,
+    );
+
+    const artichokeNode = getByLabelText('artichoke');
+    const beetrootNode = getByLabelText('beetroot');
+    const pumpkinNode = getByLabelText('pumpkin');
+
+    expect(artichokeNode).toHaveAttribute('disabled');
+    expect(beetrootNode).not.toHaveAttribute('disabled');
+    expect(pumpkinNode).not.toHaveAttribute('disabled');
+  });
+
+  test('should render as row', () => {
     const render = shallowWithTheme(
       <RadioGroup groupName="vegetable" isRow>
         <RadioButton value="artichoke" id="artichoke">
@@ -58,62 +122,9 @@ describe('<RadioGroup />', () => {
     expect(render.dive()).toMatchSnapshot();
   });
 
-  it('should render without a problem when radio is selected  and disabled', () => {
-    const render = shallowWithTheme(
-      <RadioGroup groupName="vegetable">
-        <RadioButton value="artichoke" id="artichoke">
-          artichoke
-        </RadioButton>
-        <RadioButton value="beetroot" id="beetroot">
-          beetroot
-        </RadioButton>
-        <RadioButton value="pumpkin" id="pumpkin">
-          pumpkin
-        </RadioButton>
-      </RadioGroup>,
-    );
-    expect(render.dive()).toMatchSnapshot();
-  });
-
-  it('should call change handler', () => {
-    const render = mountWithTheme(
-      <RadioGroup groupName="vegetable" selectedValue="artichoke">
-        <RadioButton value="artichoke" id="artichoke">
-          artichoke
-        </RadioButton>
-        <RadioButton value="beetroot" id="beetroot">
-          beetroot
-        </RadioButton>
-        <RadioButton value="pumpkin" id="pumpkin">
-          pumpkin
-        </RadioButton>
-      </RadioGroup>,
-    );
-    render.find('#beetroot input').simulate('change');
-    expect(render.state().selectedValue).toEqual('beetroot');
-  });
-
-  it('should call click handler', () => {
-    const render = mountWithTheme(
-      <RadioGroup groupName="vegetable" selectedValue="artichoke">
-        <RadioButton value="artichoke" id="artichoke">
-          artichoke
-        </RadioButton>
-        <RadioButton value="beetroot" id="beetroot">
-          beetroot
-        </RadioButton>
-        <RadioButton value="pumpkin" id="pumpkin">
-          pumpkin
-        </RadioButton>
-      </RadioGroup>,
-    );
-    render.find('#beetroot div[onClick]').simulate('click');
-    expect(render.state().selectedValue).toEqual('beetroot');
-  });
-
-  it('should call componentDidUpdate when props are updated', () => {
+  test('should call componentDidUpdate when props are updated', () => {
     let selectedValue = 'artichoke';
-    const render = shallowWithTheme(
+    const { getByLabelText, rerender } = render(
       <RadioGroup groupName="vegetable" selectedValue={selectedValue}>
         <RadioButton value="artichoke" id="artichoke">
           artichoke
@@ -126,7 +137,35 @@ describe('<RadioGroup />', () => {
         </RadioButton>
       </RadioGroup>,
     );
-    render.setProps({ selectedValue: 'beetroot' });
-    expect(render.state().selectedValue).toEqual('beetroot');
+    let artichokeNode = getByLabelText('artichoke');
+    let beetrootNode = getByLabelText('beetroot');
+    let pumpkinNode = getByLabelText('pumpkin');
+
+    expect(artichokeNode).toHaveAttribute('checked');
+    expect(beetrootNode).not.toHaveAttribute('checked');
+    expect(pumpkinNode).not.toHaveAttribute('checked');
+
+    selectedValue = 'beetroot';
+    rerender(
+      <RadioGroup groupName="vegetable" selectedValue={selectedValue}>
+        <RadioButton value="artichoke" id="artichoke">
+          artichoke
+        </RadioButton>
+        <RadioButton value="beetroot" id="beetroot">
+          beetroot
+        </RadioButton>
+        <RadioButton value="pumpkin" id="pumpkin">
+          pumpkin
+        </RadioButton>
+      </RadioGroup>,
+    );
+
+    artichokeNode = getByLabelText('artichoke');
+    beetrootNode = getByLabelText('beetroot');
+    pumpkinNode = getByLabelText('pumpkin');
+
+    expect(artichokeNode).not.toHaveAttribute('checked');
+    expect(beetrootNode).toHaveAttribute('checked');
+    expect(pumpkinNode).not.toHaveAttribute('checked');
   });
 });
