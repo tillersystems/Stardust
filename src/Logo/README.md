f92096b114589c3a1565ba9ff14983b18a9cb8e4
spartdev <troux@tillersystems.com>
2019-07-22 16:47:30 +0200

    feat(Logo): optimize component for better accessibility

--- src/Logo/README.md
+++ src/Logo/README.md
@@ -2,30 +2,16 @@
 
 ### Usage
 
-```jsx
-import { Logo } from '@tillersystems/stardust';
-```
-
-<!-- STORY -->
-
-### Properties
-
-- `color` - Logo color.
-- `height` - Logo height.
-- `width` - Logo width.
-
-| propName | propType | defaultValue | isRequired |
-| -------- | :------: | :----------: | :--------: |
-| `color`  | `string` |   `white`    |     -      |
-| `height` | `string` |     `28`     |     -      |
-| `width`  | `string` |    `120`     |     -      |
-
-### Example
+Display the logo of Tiller
 
 ```jsx
 import { Logo } from '@tillersystems/stardust';
 
 render() {
-  return <Logo color="red" width="230" height="76" />
+  return <Logo color="white" width="230" height="76" />
 }
 ```
+
+<!-- STORY -->
+
+<!-- PROPS -->
--- src/Logo/__tests__/Logo.test.js
+++ src/Logo/__tests__/Logo.test.js
@@ -6,12 +6,14 @@ import Logo from '..';
 describe('<Logo />', () => {
   test('should render without a problem', () => {
     const { container } = render(<Logo />);
+
     expect(container.firstChild).toMatchSnapshot();
   });
 
   test('should render with custom width & height', () => {
-    const { getByTestId } = render(<Logo width="300" height="80" />);
-    const logoNode = getByTestId('logo');
+    const { getByRole } = render(<Logo width="300" height="80" />);
+    const logoNode = getByRole('img');
+
     expect(logoNode).toHaveAttribute('width', '300');
     expect(logoNode).toHaveAttribute('height', '80');
   });
@@ -20,6 +22,7 @@ describe('<Logo />', () => {
     const color = 'hsl(217, 89%, 61%)';
     const { getByTestId } = render(<Logo color={color} />);
     const logoPathNode = getByTestId('logoPath');
+
     expect(logoPathNode).toHaveAttribute('fill', color);
   });
 });
