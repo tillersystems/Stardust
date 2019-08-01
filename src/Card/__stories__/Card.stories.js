1d8741cbb7ca74f8df774dd60e5eb3c31008e06a
Artium <artium.wasse@gmail.com>
2019-08-01 15:06:37 +0200

    feat(Card): Add Card documentation to storybook

---
+++ src/Card/__stories__/Card.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+
+import Card from '..';
+import CardReadme from '../README.md';
+
+storiesOf('Card', module)
+  .addDecorator(withKnobs)
+  .addParameters({
+    readme: {
+      // Show readme before story
+      content: CardReadme,
+    },
+  })
+  .add('with properties', () => {
+    const hasHeader = boolean('With Header', true, 'Layout');
+    const hasFooter = boolean('With Footer', true, 'Layout');
+    const isSmallHeader = boolean('Is header small', true, 'Layout');
+    return (
+      <div>
+        <Card height="350px" width="350px">
+          {hasHeader && <Card.Header small={isSmallHeader}>Header</Card.Header>}
+          <Card.Body>Hey, this is my content!</Card.Body>
+          {hasFooter && <Card.Footer>Footer</Card.Footer>}
+        </Card>
+      </div>
+    );
+  });
