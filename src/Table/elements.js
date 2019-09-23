79eae330cdf76045d7bf95926ef62bdcdaa47b00
Artikodin <artium.wasse@gmail.com>
2019-09-23 14:22:10 +0200

    feat(Table): Add scrollable behaviour to the Table component (#727)
    * feat(Table): Add scrollable behaviour to the Table component
    
    * feat(Table): Add scrollable table example to story book
    
    * docs(Table): Add doc to storybook
    
    * refactor(Table): Change naming of sortable to isSortable
    
    * test(Table): Add tests
    
    * feat(Table): Rework selected style

--- src/Table/elements.js
+++ src/Table/elements.js
@@ -1,9 +1,23 @@
 import styled, { css } from 'styled-components';
 
+const borderRight = css`
+  &:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    height: 100%;
+    border-right: 1px solid ${({ theme: { palette } }) => palette.veryLightBlue};
+  }
+`;
+
 // Table
 export const TableElement = styled.table`
+  min-width: 100%;
+
   border-collapse: collapse;
   table-layout: fixed;
+  position: relative;
 `;
 
 export const Row = styled.tr`
@@ -12,6 +26,9 @@ export const Row = styled.tr`
   background: ${({ theme: { palette } }) => palette.white};
   &:last-child {
     border-bottom: 0;
+  }
+  &:first-child {
+    border-top: 0;
   }
 `;
 
@@ -35,32 +52,52 @@ export const TableHeader = styled.thead`
 `;
 
 export const TableHeaderCell = styled.th`
+  position: relative;
   height: 4.4rem;
+  padding: 0 0.5rem;
+
+  background-color: ${({ theme: { palette } }) => palette.white};
+
+  text-align: ${({ align }) => align || 'left'};
+
+  &:before {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    border-bottom: 1px solid ${({ theme: { palette } }) => palette.veryLightBlue};
+  }
 
   &:first-child {
     padding-left: 3rem;
+    padding-right: 0.5rem;
+    z-index: 2;
+    left: 0;
+    ${({ isScrollable }) => isScrollable && borderRight}
   }
+
   &:last-child {
     padding-right: 3rem;
+    padding-left: 0.5rem;
   }
 
-  text-align: ${({ align }) => align || 'left'};
-`;
-
-export const HeaderSortingContainer = styled.div`
-  display: flex;
-  align-items: center;
-
-  ${({ align }) =>
-    align === 'right' &&
+  ${({ isScrollable }) =>
+    isScrollable &&
     css`
-      justify-content: flex-end;
-    `};
+      position: sticky;
+      top: 0;
+      z-index: 1;
+    `}
 
-  cursor: pointer;
+  ${({ isSortable }) =>
+    isSortable &&
+    css`
+      cursor: pointer;
+    `}
 `;
 
-export const HeaderLabel = styled.div`
+export const HeaderLabel = styled.span`
   margin-right: 0.8rem;
   user-select: none;
 `;
@@ -68,13 +105,15 @@ export const HeaderLabel = styled.div`
 // Table Body
 export const Body = styled.tbody`
   td {
-    padding: 1.6rem 0;
+    padding: 1.6rem 0.5rem;
 
     font-weight: ${({
       theme: {
         fonts: { weight },
       },
     }) => weight.thick};
+
+    min-width: 10rem;
 
     &:first-child {
       padding-left: 3rem;
@@ -95,17 +134,94 @@ export const BodyRow = styled(Row)`
       }
     `};
 
-  ${({ selected }) =>
-    selected &&
-    css`
-      box-shadow: inset 3px 0px 0 0px ${({ theme: { palette } }) => palette.primary.default};
-    `};
-
   ${({ striped }) =>
     striped &&
     css`
+      &:nth-child(even) > th,
       &:nth-child(even) {
         background: ${({ theme: { palette } }) => palette.paleGrey};
       }
     `};
+
+  ${({ selected }) =>
+    selected &&
+    css`
+      &:nth-child(1n) > th,
+      &:nth-child(1n) {
+        background: ${({ theme: { palette } }) => palette.veryLightGrey};
+        box-shadow: inset 3px 0px 0 0px ${({ theme: { palette } }) => palette.primary.default};
+      }
+    `};
+`;
+
+export const RowHeader = styled.th`
+  ${({ isScrollable }) =>
+    isScrollable &&
+    css`
+      position: sticky;
+      left: 0;
+      ${borderRight}
+    `}
+
+  text-align: ${({ align }) => align || 'left'};
+  font-weight: ${({ theme: { fonts } }) => fonts.weight.thick};
+
+  padding: 1.8rem 0.5rem 1.8rem 3rem;
+  min-width: 15rem;
+  background-color: ${({ theme: { palette } }) => palette.white};
+`;
+
+// Table Footer
+export const Footer = styled.tfoot`
+  th,
+  td {
+    ${({ isScrollable }) =>
+      isScrollable &&
+      css`
+        bottom: 0;
+        position: sticky;
+      `}
+
+    &:after {
+      content: '';
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 100%;
+      border-top: 1px solid ${({ theme: { palette } }) => palette.veryLightBlue};
+    }
+
+    padding: 1.8rem 0.5rem;
+    background-color: ${({ theme: { palette } }) => palette.white};
+
+    color: ${({ theme: { palette } }) => palette.primary.default};
+    font-weight: ${({ theme: { fonts } }) => fonts.weight.thick};
+  }
+
+  th {
+    ${({ isScrollable }) =>
+      isScrollable &&
+      css`
+        left: 0;
+        z-index: 1;
+        ${borderRight}
+      `}
+
+    padding: 1.8rem 0.5rem 1.8rem 3rem;
+  }
+
+  td:last-of-type {
+    padding: 1.8rem 3rem 1.8rem 0.5rem;
+  }
+`;
+
+// Table Container
+export const Container = styled.div`
+  height: ${({ height }) => height};
+  ${({ isScrollable }) =>
+    isScrollable &&
+    css`
+      overflow: scroll;
+    `}
+  position: relative;
 `;
--- src/Table/index.js
+++ src/Table/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Icon, Theme } from '..';
 import compare from '../helpers/compare';
 import {
-  HeaderSortingContainer,
+  Container,
   HeaderLabel,
   Body,
   BodyRow,
@@ -12,6 +12,8 @@ import {
   TableElement,
   TableHeader,
   TableHeaderCell,
+  RowHeader,
+  Footer,
 } from './elements';
 
 /** Lookup object for next sorting direction. */
@@ -89,7 +91,7 @@ class Table extends PureComponent {
    * @return {jsx}
    */
   renderHeader() {
-    const { colsDef } = this.props;
+    const { colsDef, isScrollable } = this.props;
     const {
       sort: { index, direction },
     } = this.state;
@@ -97,23 +99,24 @@ class Table extends PureComponent {
     return (
       <TableHeader>
         <Row>
-          {colsDef.map(({ title, sortable, align }, columnIndex) => (
+          {colsDef.map(({ title, isSortable, align }, columnIndex) => (
             <TableHeaderCell
+              isScrollable={isScrollable}
+              isSortable={isSortable}
+              scope="col"
               key={`${title}-${columnIndex}`}
               align={align}
-              onClick={() => (sortable ? this.handleSortingClick(columnIndex) : undefined)}
+              onClick={() => (isSortable ? this.handleSortingClick(columnIndex) : undefined)}
             >
-              <HeaderSortingContainer align={align}>
-                <HeaderLabel>{title}</HeaderLabel>
-                {sortable && (
-                  <Icon
-                    name={sortingDirectionToIconName[columnIndex == index ? direction : 'none']}
-                    color={Theme.palette.mediumGrey}
-                    width="10px"
-                    height="10px"
-                  />
-                )}
-              </HeaderSortingContainer>
+              <HeaderLabel>{title}</HeaderLabel>
+              {isSortable && (
+                <Icon
+                  name={sortingDirectionToIconName[columnIndex == index ? direction : 'none']}
+                  color={Theme.palette.mediumGrey}
+                  width="10px"
+                  height="10px"
+                />
+              )}
             </TableHeaderCell>
           ))}
         </Row>
@@ -130,6 +133,7 @@ class Table extends PureComponent {
     const {
       colsDef,
       data,
+      isScrollable,
       rowsDef: { selectable },
       striped,
     } = this.props;
@@ -181,14 +185,51 @@ class Table extends PureComponent {
             striped={striped}
             onClick={() => this.handleRowSelect(item, key)}
           >
-            {colsDef.map(({ value, format, align }, columnIndex) => (
-              <td key={`column-${columnIndex}`} align={align}>
-                {format ? format(value(item, index), index) : value(item, index)}
-              </td>
-            ))}
+            {colsDef.map(({ isRowHeader, value, format, align }, columnIndex) =>
+              isRowHeader ? (
+                <RowHeader
+                  align={align}
+                  isScrollable={isScrollable}
+                  key={`column-header-${columnIndex}`}
+                >
+                  {value(item, index)}
+                </RowHeader>
+              ) : (
+                <td key={`column-${columnIndex}`} align={align}>
+                  {format ? format(value(item, index), index) : value(item, index)}
+                </td>
+              ),
+            )}
           </BodyRow>
         ))}
       </Body>
+    );
+  }
+
+  /**
+   * Renders the footer of the table.
+   *
+   * @return {jsx}
+   */
+  renderFooter() {
+    const { colsDef, dataTotal, isScrollable } = this.props;
+
+    return (
+      <Footer data-testid="footer-row" isScrollable={isScrollable}>
+        <tr>
+          {colsDef.map(({ isRowHeader, total, format, align }, columnIndex) =>
+            isRowHeader ? (
+              <th scope="row" key={`total-header-${columnIndex}`} align={align}>
+                {format ? format(total(dataTotal)) : total(dataTotal)}
+              </th>
+            ) : (
+              <td key={`total-${columnIndex}`} align={align}>
+                {format ? format(total(dataTotal)) : total(dataTotal)}
+              </td>
+            ),
+          )}
+        </tr>
+      </Footer>
     );
   }
 
@@ -198,18 +239,33 @@ class Table extends PureComponent {
    * @return {jsx}
    */
   render() {
-    const { width } = this.props;
+    const { dataTotal, height, isScrollable, width } = this.props;
 
     return (
-      <TableElement width={width}>
-        {this.renderHeader()}
-        {this.renderBody()}
-      </TableElement>
+      <Container data-testid="table-container" height={height} isScrollable={isScrollable}>
+        <TableElement width={isScrollable ? 'initial' : width}>
+          {this.renderHeader()}
+          {this.renderBody()}
+          {dataTotal && this.renderFooter()}
+        </TableElement>
+      </Container>
     );
   }
 }
 
-const { array, arrayOf, bool, func, node, number, oneOf, oneOfType, shape, string } = PropTypes;
+const {
+  array,
+  arrayOf,
+  bool,
+  func,
+  node,
+  number,
+  object,
+  oneOf,
+  oneOfType,
+  shape,
+  string,
+} = PropTypes;
 
 /** Prop types. */
 Table.propTypes = {
@@ -217,10 +273,12 @@ Table.propTypes = {
   colsDef: arrayOf(
     shape({
       align: oneOf(['left', 'center', 'right']),
+      isRowHeader: bool,
       filteredBy: func,
       format: func,
-      sortable: bool,
+      isSortable: bool,
       title: node.isRequired,
+      total: func,
       value: func.isRequired,
       width: oneOfType([string, number]),
     }),
@@ -228,6 +286,22 @@ Table.propTypes = {
 
   /** Data to display */
   data: array.isRequired,
+
+  /**
+   * This props going to add a fixed row at the bottom of the table.
+   * Usually this props is used to display the total of each column.
+   * */
+  dataTotal: object,
+
+  /** Height of the table use this props only is the table is scrollable */
+  height: string,
+
+  /**
+   * Define if the table is scrollable or not.
+   * When the table is scrollable the column header stick to the top and the row header stick to the left.
+   * The table body going to scroll in both axies x and y depending of the content overflow.
+   **/
+  isScrollable: bool,
 
   /** Rows definition */
   rowsDef: shape({
@@ -238,12 +312,15 @@ Table.propTypes = {
   /** Whether rows should alternate color or not */
   striped: bool,
 
-  /** Width of the table */
+  /** Width of the table use this props only is the table is not scrollable*/
   width: string,
 };
 
 /** Default props. */
 Table.defaultProps = {
+  dataTotal: null,
+  height: 'initial',
+  isScrollable: false,
   rowsDef: {
     onSelect: () => {},
     selectable: false,
