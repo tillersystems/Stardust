db785a4b15f85f61b2d41660f0a90aa016c66128
spartdev <troux@tillersystems.com>
2019-10-10 18:47:18 +0200

    test(Modal): add missing tests

--- src/Modal/Footer/__tests__/Footer.test.js
+++ src/Modal/Footer/__tests__/Footer.test.js
@@ -9,9 +9,15 @@ describe('<Modal.Footer />', () => {
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  test('should render with another alignment', () => {
-    const { container } = render(<Footer alignment="center">Footer</Footer>);
+  test.each`
+    value              | props
+    ${'flex-start'}    | ${'left'}
+    ${'flex-end'}      | ${'right'}
+    ${'center'}        | ${'center'}
+    ${'space-between'} | ${'spaced'}
+  `('should render with $props alignment', ({ value, props }) => {
+    const { container } = render(<Footer alignment={props}>Footer</Footer>);
 
-    expect(container.firstChild).toHaveStyleRule('justify-content', 'center');
+    expect(container.firstChild).toHaveStyleRule('justify-content', value);
   });
 });
