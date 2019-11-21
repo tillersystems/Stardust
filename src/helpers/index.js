bfc4f91fcd207535dc2316d433cd2fd69dfcfaa8
Sun-tea <xthi.ngu@gmail.com>
2019-11-21 11:45:31 +0100

    feat(helpers): add formatting numbers for unit from sonar

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,2 @@
 export { dark, light, getLightness, setColorDark, whiteOpacity } from './colors';
-export { formatCompactedNumber, formatNumber } from './formatting';
+export { formatCompactedNumber, formatNumber, formatUnitNumber, UNIT_SYMBOLS } from './formatting';
